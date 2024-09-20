6bb02f41746df94e0dfea421dfc2cb0b14b7c5df
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2024-09-20 13:17:05 +0530

    Rendering List in React

--- basicapp/src/App.js
+++ basicapp/src/App.js
@@ -1,43 +1,47 @@
-import React  from 'react'
+import React ,{useState} from 'react'
 
 import NewExpenses from './components/NewExpenses/NewExpenses';
 
 import Expenses from "./components/Expenses/Expenses";
 
+  
+let DUMMY_EXPENSE = [
+  {
+    id: "e1",
+    title: "School Fee",
+    amount: 250,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: "e2",
+    title: "Books",
+    amount: 350,
+    date: new Date(2021, 10, 12),
+  },
+  {
+    id: "e3",
+    title: "House rent",
+    amount: 2500,
+    date: new Date(2024, 5, 12),
+  },
+  {
+    id: "e4",
+    title: "Travel",
+    amount: 500,
+    date: new Date(2022, 8, 14),
+  },
+];
+
 const App = () => {
   // let expenceDate = new Date(2021, 3, 28);
   // let expenceTitle = "School Fee";
   // let expenceAmount = 300;
-
-  let expense = [
-    {
-      id: "e1",
-      title: "School Fee",
-      amount: 250,
-      date: new Date(2021, 5, 12),
-    },
-    {
-      id: "e2",
-      title: "Books",
-      amount: 350,
-      date: new Date(2021, 10, 12),
-    },
-    {
-      id: "e3",
-      title: "House rent",
-      amount: 2500,
-      date: new Date(2024, 5, 12),
-    },
-    {
-      id: "e4",
-      title: "Travel",
-      amount: 500,
-      date: new Date(2022, 8, 14),
-    },
-  ];
+   
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE)
     
    const addExpenseHandler = (expense) => {
-    console.log(expense);
+     const updatedExpense = [expense, ...expenses]
+     setExpenses(updatedExpense)
     
    }
   return (
